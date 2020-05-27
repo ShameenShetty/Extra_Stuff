07a02169b12f029b66dea06deb2825ba01084c81
ShameenShetty <57455717+ShameenShetty@users.noreply.github.com>
2020-05-27 13:31:04 -0500

    Add files via upload
    added solution for edabit prob --> https://edabit.com/challenge/HrQoXJYqpYZ2Rqvtb

---
+++ Programming Problems/Product_of_Digits_of_Sum.py
@@ -0,0 +1,89 @@
+# Shameen Shetty
+
+'''
+    We create a function that takes numbers as arguments, adds them together, and returns
+     the product of digits until the answer is only 1 digit long.
+
+    For example, 
+
+    sum_dig_prod(16, 28) ➞ 6
+    # 16 + 28 = 44
+    # 4 * 4 =  16
+    # 1 * 6 = 6
+
+    sum_dig_prod(0) ➞ 0
+
+    sum_dig_prod(1, 2, 3, 4, 5, 6) ➞ 2
+'''
+
+# Gets the length of the number, this way we can check if the result
+# of our operations is 1 digit or not
+def getLen(num):
+    return len(str(num))
+
+# If we enter multiple args eg. (2, 3, 4) then we sum those numbers
+# or if there is a single arg, we multiply the individual digits of those
+# numbers.
+# Then we return either the sum or the prod, depending on which operation was done
+def get_Sum_or_Prod(*args):
+    sum = 0
+    prod = 1
+
+    isSum = False
+
+    if len(args) > 1:
+        # Since there are more than one arguments entered,
+        # we will sum the numbers first
+        isSum = True
+        for num in args:
+            sum += num
+    
+    elif len(args) == 1:
+        # Since there is just one arg that is passed
+        # we will first convert that integer into a string,
+        # then get all the digits of that number and store it into a list
+        # listOfDigits, and then multiply all those digits and store it in prod
+        stringNum = str(*args)
+        listOfDigits = []
+        for digit in stringNum:
+            listOfDigits.append(int(digit))
+
+        for digit in listOfDigits:
+            prod *= digit
+
+    # A flag, isSum checks if we did a sum or not, this way we can return
+    # either the sum or the product.    
+    if isSum:
+        return sum
+    else:
+        return prod
+
+# Our function sum_digits_prod that finds the product of digits until the answer is only 1 digit long
+# which we check by first getting the 'first sum' by passing all the arbitrary arguments, then
+# we check if the length of firstSum is 1. If it isn't, then we recurively pass it through our
+# sum_digits_prod() function until we get a result that is only 1 digit long.
+
+'''
+    Function: sum_dig_prod
+    
+    Parameters: 1) *args, which takes in an arbitrary number of arguments
+    
+    Returns: None
+
+    Description: This function takes in a arbitrary number of numbers as arguments,
+    adds them together, and returns the product of digits until the answer is only 1 digit long.
+'''
+def sum_dig_prod(*args):
+    firstSum = get_Sum_or_Prod(*args)
+    
+    if getLen(firstSum) != 1:
+        sum_dig_prod(firstSum)
+    else:
+        print(f"sum_dig_prod() -> {firstSum}")
+
+
+
+if __name__ == "__main__":
+    sum_dig_prod(16, 28)
+    sum_dig_prod(0)
+    sum_dig_prod(1, 2, 3, 4, 5, 6)
